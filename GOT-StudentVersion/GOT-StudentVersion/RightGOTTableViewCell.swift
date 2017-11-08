cb9317fb236283e6aeacc978e91d808c918030a2
Maryann Yin <maryannyin@ac.c4q.nyc>
2017-11-07 21:39:37 -0500

    left GOT table view cell is working

---
+++ GOT-StudentVersion/GOT-StudentVersion/RightGOTTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RightGOTTableViewCell.swift
+//  GOT-StudentVersion
+//
+//  Created by Maryann Yin on 11/4/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class RightGOTTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
@@ -2,24 +2,53 @@
 //  ViewController.swift
 //  GOT-StudentVersion
 //
-//  Created by C4Q  on 11/2/17.
+//  Created by Maryann Yin on 11/4/17.
 //  Copyright © 2017 C4Q . All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet var gotTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        gotTableView.delegate = self
+        gotTableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return GOTEpisode.allEpisodes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = gotTableView.dequeueReusableCell(withIdentifier: "LeftGOTCell", for: indexPath) as! LeftGOTTableViewCell
+        let gotEpisodeInfo = GOTEpisode.allEpisodes[indexPath.row]
+        let gotSeasonNum = gotEpisodeInfo.season
+        let gotEpisodeNum = gotEpisodeInfo.number
+        let gotEpisodeTitle = gotEpisodeInfo.name
+        let gotEpisodeImageID = gotEpisodeInfo.originalImageID
+        cell.episodeTitleLabel.text = gotEpisodeTitle
+        cell.seasonAndEpisodeNumberLabel.text = "S: \(gotSeasonNum) E: \(gotEpisodeNum)"
+        if let gotEpisodePic = UIImage(named: gotEpisodeImageID) {
+                    cell.gotEpisodeImageView.image = gotEpisodePic
+        }
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    /*
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
