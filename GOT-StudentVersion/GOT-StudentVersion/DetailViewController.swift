9c2d5f0ecae33a58f1c1ef66b207f86b03855914
Maryann Yin <maryannyin@ac.c4q.nyc>
2017-11-08 00:57:17 -0500

    most of the GOT app is working

---
+++ GOT-StudentVersion/GOT-StudentVersion/DetailViewController.swift
@@ -0,0 +1,67 @@
+//
+//  DetailViewController.swift
+//  GOT-StudentVersion
+//
+//  Created by Maryann Yin on 11/7/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var gotEpisodeTitleLabel: UILabel!
+    
+    @IBOutlet var gotSeasonNumberLabel: UILabel!
+    
+    @IBOutlet var gotEpisodeNumberLabel: UILabel!
+    
+    @IBOutlet var gotEpisodeRuntimeLabel: UILabel!
+    
+    @IBOutlet var gotEpisodeAirDate: UILabel!
+    
+    @IBOutlet var gotEpisodeSummary: UILabel!
+    
+    @IBOutlet var gotEpisodeImageView: UIImageView!
+    
+    //disconnect this outlet and reconnect it as a UILabel
+    
+    //add the outlet for the summary
+    
+    //continue this pattern for the image view, season number, episode number, episode run timem, episode air date, episode summary
+    //(1) declare the empty string (2) in view will appear function, set the text of the relevant label
+    
+    var episodeTitle = ""
+    
+    var seasonNumber = ""
+    
+    var episodeNumber = ""
+    
+    var episodeRunTime = ""
+    
+    var episodeAirDate = ""
+    
+    var episodeSummary = ""
+    
+    var episodeImageID = ""
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        gotEpisodeTitleLabel.text = episodeTitle
+  //      gotSeasonNumberLabel.text = seasonNumber
+        gotEpisodeNumberLabel.text = episodeNumber
+  //      gotEpisodeRuntimeLabel.text = episodeRunTime
+ //       gotEpisodeAirDate.text = episodeAirDate
+//        gotEpisodeSummary.text = episodeSummary
+//        if let gotEpisodeImage = UIImage(named: episodeImageID) {
+//            gotEpisodeImageView.image = gotEpisodeImage
+ //       }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
@@ -12,38 +12,45 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet var gotTableView: UITableView!
  
-    let gotSeasonSections = ["Season 1", "Season 2", "Season 3", "Season 4", "Season 5", "Season 6", "Season 7"]
+    static let gotSeasonSections = ["Season 1", "Season 2", "Season 3", "Season 4", "Season 5", "Season 6", "Season 7"]
+    
+    var gotEpisodesBySeason: [[GOTEpisode]] = Array(repeating: [], count: ViewController.gotSeasonSections.count)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         gotTableView.delegate = self
         gotTableView.dataSource = self
+        
+        for episode in GOTEpisode.allEpisodes {
+                let seasons = episode.season
+                gotEpisodesBySeason[seasons - 1].append(episode)
+        }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return gotSeasonSections.count
+        return gotEpisodesBySeason.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return GOTEpisode.allEpisodes.count
+        return gotEpisodesBySeason[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let leftCell = gotTableView.dequeueReusableCell(withIdentifier: "LeftGOTCell", for: indexPath) as! LeftGOTTableViewCell
         let rightCell = gotTableView.dequeueReusableCell(withIdentifier: "RightGOTCell", for: indexPath) as! RightGOTTableViewCell
-        let gotEpisodeInfo = GOTEpisode.allEpisodes[indexPath.row]
+        let rowToSetUp = indexPath.row
+        let currentSection = indexPath.section //NEW
+        let gotEpisodeInfo = gotEpisodesBySeason[currentSection][rowToSetUp] //NEW
         let gotSeasonNum = gotEpisodeInfo.season
         let gotEpisodeNum = gotEpisodeInfo.number
         let gotEpisodeTitle = gotEpisodeInfo.name
         let gotEpisodeImageID = gotEpisodeInfo.originalImageID
-        if indexPath.row % 2 == 0 {
+        if indexPath.section % 2 == 0 {
             rightCell.gotEpisodeTitleLabel.text = gotEpisodeTitle
             rightCell.gotSeasonAndEpisodeNumberLabel.text = "S: \(gotSeasonNum) E: \(gotEpisodeNum)"
             if let gotEpisodeImage = UIImage(named: gotEpisodeImageID) {
                  rightCell.gotEpisodeImageView.image = gotEpisodeImage
             }
-            
-            
 
             return rightCell
         } else {
@@ -55,15 +62,48 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             return leftCell
         }
     }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return ViewController.gotSeasonSections[section]
+        case 1:
+            return ViewController.gotSeasonSections[section]
+        case 2:
+            return ViewController.gotSeasonSections[section]
+        case 3:
+            return ViewController.gotSeasonSections[section]
+        case 4:
+            return ViewController.gotSeasonSections[section]
+        case 5:
+            return ViewController.gotSeasonSections[section]
+        case 6:
+            return ViewController.gotSeasonSections[section]
+        default:
+            return nil
+        }
+    }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "RightGOTCellSegue" || segue.identifier == "LeftGOTCellSegue" {
+            var detailedVC = segue.destination as! DetailViewController
+            if let indexPath = self.gotTableView.indexPathForSelectedRow {
+                var gotEpisode = gotEpisodesBySeason[indexPath.section][indexPath.row]
+                detailedVC.episodeTitle = gotEpisode.name
+  //              detailedVC.seasonNumber = String(gotEpisode.season)
+    //            detailedVC.episodeNumber = String(gotEpisode.number)
+   //             detailedVC.episodeRunTime = String(gotEpisode.runtime)
+  //              detailedVC.episodeAirDate = gotEpisode.airdate
+ //               detailedVC.episodeSummary = gotEpisode.summary
+//                detailedVC.episodeImageID = gotEpisode.originalImageID
+            }
+        }
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
