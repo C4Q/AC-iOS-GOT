a22030f1c72970cf20598f3834a4df0e8be4ffe9
Nathan Rosario <nathanrosario@ac.c4q.nyc>
2017-11-08 10:31:09 -0500

    Updates

--- GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
@@ -8,34 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UISearchResultsUpdating {
 
     var episodeArray = [GOTEpisode]()
     
     @IBOutlet weak var tableView: UITableView!
     
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    var filteredepisodeArr: [GOTEpisode] {
+        guard let searchTerm = searchTerm, searchTerm != "" else {
+            return episodeArray
+        }
+        guard let scopeTitles = self.searchController.searchBar.scopeButtonTitles else {
+            return episodeArray
+        }
+        let selectedIndex = self.searchController.searchBar.selectedScopeButtonIndex
+        let filteringCriteria = scopeTitles[selectedIndex]
+        switch filteringCriteria {
+        case "Title":
+            return episodeArray.filter{$0.name.lowercased().contains(searchTerm.lowercased())}
+        case "Description":
+            return episodeArray.filter{$0.summary.lowercased().contains(searchTerm.lowercased())}
+        default:
+            return episodeArray
+        }
+    }
+    var searchTerm: String? {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.delegate = self
         self.tableView.dataSource = self
         loadData()
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.placeholder = "Search Episodes"
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+        searchController.searchBar.scopeButtonTitles = ["Title", "Description"]
+        searchController.searchBar.delegate = self
     }
     func loadData() {
         self.episodeArray = GOTEpisode.allEpisodes
     }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        self.searchTerm = searchController.searchBar.text
+    }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.searchTerm = searchBar.text
+    }
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        tableView.reloadData()
+    }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 7
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let season = section + 1
-        let episodesInSeason = episodeArray.filter{$0.season == season}
+        let episodesInSeason = filteredepisodeArr.filter{$0.season == season}
         return episodesInSeason.count
-//        return episodeArray.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let season = indexPath.section + 1
-        let episodesInSeason = episodeArray.filter{$0.season == season}
+        let episodesInSeason = filteredepisodeArr.filter{$0.season == season}
         
         let episode = episodesInSeason[indexPath.row]
         guard episode.season % 2 == 0 else {
@@ -60,7 +101,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        var currentSeason = episodeArray.filter{$0.season == tableView.indexPathForSelectedRow!.section + 1 }
+        var currentSeason = filteredepisodeArr.filter{$0.season == tableView.indexPathForSelectedRow!.section + 1 }
         switch segue.identifier! {
         case "leftSegue":
             if let destination = segue.destination as? SummaryViewController {
