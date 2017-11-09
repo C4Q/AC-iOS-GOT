f30422b30035092caed056c67198a70acff953d2
Margaret Chan <margaret.helen.chan@gmail.com>
2017-11-08 21:33:35 -0500

    submission

---
+++ GOT-StudentVersion/GOT-StudentVersion/LeftTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LeftTableViewCell.swift
+//  GOT-StudentVersion
+//
+//  Created by C4Q on 11/8/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class LeftTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var episodeImage: UIImageView!
+    @IBOutlet weak var episodeTitle: UILabel!
+    @IBOutlet weak var episodeNumber: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+
+}
