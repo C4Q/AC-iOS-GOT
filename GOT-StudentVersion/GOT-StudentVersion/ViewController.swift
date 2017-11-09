d1f2963ef830478613fb36d6a0db083b265a066b
Izza Nadeem <izza.nadeem99@gmail.com>
2017-11-09 09:58:33 -0500

    .

--- GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
@@ -9,28 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-     var GOTDetail: GOTEpisode?
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    var GOTDetail: GOTEpisode?
+    
     var allGOTEpisodes = GOTEpisode.allEpisodes
     var seasons: [(season: Int, episode: Int)] = []
     //list of all seasons - 1-7, no repeats
     
+    
+    
+    
+    // MARK - DATASOURCE METHODS
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return gotSeasons[section].count
     }
-//
-    
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        
         let section = indexPath.section
         let season = gotSeasons[section]
-        
         let row = indexPath.row
         let episode = season[row]
-        
-        
-        
-//        let episode = allGOTEpisodes[indexPath.row]
         let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "leftCell", for: indexPath)
         tableViewCell.imageView?.image = UIImage(named: episode.mediumImageID.description)
         tableViewCell.textLabel?.text = episode.name
@@ -43,12 +43,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return gotSeasons.count
         }
    
+    // MARK - HEADER
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         
         let seasonTitle = "Season " + (section + 1).description
         
         return seasonTitle
     }
+    
     @IBOutlet weak var tableViewGOT: UITableView!
     var gotSeasons: [[GOTEpisode]] = []
     override func viewDidLoad() {
@@ -74,8 +76,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     
     
-    
-    
+
+    // MARK - Segue
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
        if let destination = segue.destination as? EpisodeDescriptionViewController {
         var season = tableViewGOT.indexPathForSelectedRow?.section
