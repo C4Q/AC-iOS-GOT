2725cc678d9597245c34ca4194ebb5d0c8d77bd7
Michelle <Michelle.cueva@gmail.com>
2019-08-10 19:19:12 -0400

    Creates rows

--- GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let seasonOne = GOTEpisode.seasonOne
+    let seasonTwo = GOTEpisode.seasonTwo
+    let seasonThree = GOTEpisode.seasonThree
+    let seasonFour = GOTEpisode.seasonFour
+    let seasonFive = GOTEpisode.seasonFive
+    let seasonSix = GOTEpisode.seasonSix
+    let seasonSeven = GOTEpisode.seasonSeven
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +28,40 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return seasonOne.count
+        case 1:
+            return seasonTwo.count
+        case 2:
+            return seasonThree.count
+        case 3:
+            return seasonFour.count
+        case 4:
+            return seasonFive.count
+        case 5:
+            return seasonSix.count
+        case 6:
+            return seasonSeven.count
+        default:
+            return -1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        }
 }
 
