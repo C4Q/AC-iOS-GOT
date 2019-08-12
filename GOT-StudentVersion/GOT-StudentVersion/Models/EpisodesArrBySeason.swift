05384aef947537d5a9df827426d9157481b9b094
Angela Garrovillas <angelagarrovillas@Angelas-MacBook-Pro.local>
2019-08-12 09:47:04 -0400

    makes model and table cell folders

---
+++ GOT-StudentVersion/GOT-StudentVersion/Models/EpisodesArrBySeason.swift
@@ -0,0 +1,16 @@
+//
+//  EpisodesArrBySeason.swift
+//  GOT-StudentVersion
+//
+//  Created by Angela Garrovillas on 8/12/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import Foundation
+var gotSeason1 = filterBySeason(got: GOTEpisode.allEpisodes, season: 1)
+var gotSeason2 = filterBySeason(got: GOTEpisode.allEpisodes, season: 2)
+var gotSeason3 = filterBySeason(got: GOTEpisode.allEpisodes, season: 3)
+var gotSeason4 = filterBySeason(got: GOTEpisode.allEpisodes, season: 4)
+var gotSeason5 = filterBySeason(got: GOTEpisode.allEpisodes, season: 5)
+var gotSeason6 = filterBySeason(got: GOTEpisode.allEpisodes, season: 6)
+var gotSeason7 = filterBySeason(got: GOTEpisode.allEpisodes, season: 7)
---
+++ GOT-StudentVersion/GOT-StudentVersion/TabelViewCells/cellTwoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  cellTwoTableViewCell.swift
+//  GOT-StudentVersion
+//
+//  Created by Angela Garrovillas on 8/12/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class cellTwoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
