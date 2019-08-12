5ba40d367946a3de2603bd6f2202ec4828f06060
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-08-12 16:23:42 -0400

    makes static constants for each season and sets number of sections, title for header, and number of rows per section functions

--- GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
@@ -33,15 +33,49 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        <#code#>
+        return 7
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        <#code#>
+        switch section {
+            case 0:
+                return "Season One"
+            case 1:
+                return "Season Two"
+            case 2:
+                return "Season Three"
+            case 3:
+                return "Season Four"
+            case 4:
+                return "Season Five"
+            case 5:
+                return "Season Six"
+            case 6:
+                return "Season Seven"
+            default:
+                return "N/A"
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        switch section {
+        case 0:
+            return GOTEpisode.seasonOne.count
+        case 1:
+            return GOTEpisode.seasonTwo.count
+        case 2:
+            return GOTEpisode.seasonThree.count
+        case 3:
+            return GOTEpisode.seasonFour.count
+        case 4:
+            return GOTEpisode.seasonFive.count
+        case 5:
+            return GOTEpisode.seasonSix.count
+        case 6:
+            return GOTEpisode.seasonSeven.count
+        default:
+            return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
