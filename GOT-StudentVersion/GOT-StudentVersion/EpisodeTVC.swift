12c00167479dda9156c2fac029310fce880350c0
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-08-12 16:54:21 -0400

    sets table view in main story board, creates oulet from board to VC, creates custom cells and labels in custom cell to display episode name and season, sets labels oulets to cell class

---
+++ GOT-StudentVersion/GOT-StudentVersion/EpisodeTVC.swift
@@ -0,0 +1,28 @@
+//
+//  EpisodeTVC.swift
+//  GOT-StudentVersion
+//
+//  Created by Mariel Hoepelman on 8/12/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class EpisodeTVC: UITableViewCell {
+
+    @IBOutlet weak var episodeNameLabel: UILabel!
+    
+    @IBOutlet weak var seasonNumberLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
@@ -5,20 +5,37 @@
 //  Created by C4Q  on 11/2/17.
 //  Copyright © 2017 C4Q . All rights reserved.
 //
-
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var gotTableView: UITableView!
+    
+    let episode = GOTEpisode.allEpisodes
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        gotTableView.delegate = self
+        gotTableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
