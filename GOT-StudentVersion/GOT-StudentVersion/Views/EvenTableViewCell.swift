8dc1ba8dec4ed613a21879763148b98f06bde97d
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-08-12 17:05:16 -0400

    creates cocotouch files for two custom cells. Creates folders for models, views, and controller

---
+++ GOT-StudentVersion/GOT-StudentVersion/Views/EvenTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EvenTableViewCell.swift
+//  GOT-StudentVersion
+//
+//  Created by Sunni Tang on 8/12/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class EvenTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ GOT-StudentVersion/GOT-StudentVersion/Views/OddTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OddTableViewCell.swift
+//  GOT-StudentVersion
+//
+//  Created by Sunni Tang on 8/12/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class OddTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
