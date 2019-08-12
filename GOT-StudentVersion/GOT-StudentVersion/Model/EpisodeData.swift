0308f1eb83cd43e7adc9b17789ceebaabdd48436
alysonabril <alysonabril@pursuit.org>
2019-08-12 14:42:54 -0400

    added, episodedata file, organized files

--- GOT-StudentVersion/GOT-StudentVersion/Model/EpisodeData.swift
+++ GOT-StudentVersion/GOT-StudentVersion/Model/EpisodeData.swift
@@ -1,34 +1,14 @@
 //
-//  GOTEpisdoe.swift
+//  EpisodeInfo.swift
 //  GOT-StudentVersion
 //
-//  Created by C4Q  on 11/2/17.
-//  Copyright © 2017 C4Q . All rights reserved.
+//  Created by Alyson Abril on 8/12/19.
+//  Copyright © 2019 C4Q . All rights reserved.
 //
 
 import Foundation
 
-class GOTEpisode {
-    var airdate: String
-    var id: Int
-    var name: String
-    var number: Int
-    var season: Int
-    var runtime: Int
-    var summary: String
-    var mediumImageID: String
-    var originalImageID: String
-    init(airdate: String, id: Int, name: String, number: Int, season: Int, runtime: Int, summary: String, mediumImageID: String, originalImageID: String) {
-        self.airdate = airdate
-        self.id = id
-        self.name = name
-        self.number = number
-        self.season = season
-        self.runtime = runtime
-        self.summary = summary
-        self.mediumImageID = mediumImageID
-        self.originalImageID = originalImageID
-    }
+struct EpisodeData {
     static let allEpisodes = [
         GOTEpisode(airdate: "2011-04-17", id: 4952, name: "Winter is Coming", number: 1, season: 1, runtime: 60, summary: "Lord Eddard Stark, ruler of the North, is summoned to court by his old friend, King Robert Baratheon, to serve as the King's Hand. Eddard reluctantly agrees after learning of a possible threat to the King's life. Eddard's bastard son Jon Snow must make a painful decision about his own future, while in the distant east Viserys Targaryen plots to reclaim his father's throne, usurped by Robert, by selling his sister in marriage.", mediumImageID: "2668", originalImageID: "2668"),
         GOTEpisode(airdate: "2011-04-24", id: 4953, name: "The Kingsroad", number: 2, season: 1, runtime: 60, summary: "An incident on the Kingsroad threatens Eddard and Robert's friendship. Jon and Tyrion travel to the Wall, where they discover that the reality of the Night's Watch may not match the heroic image of it.", mediumImageID: "2669", originalImageID: "2669"),
