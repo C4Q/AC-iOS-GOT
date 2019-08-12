f4e475d1738d6c5774acf22ce1ce8ebd17d4edfe
Jason Ruan <jasonruan@pursuit.org>
2019-08-12 16:20:28 -0400

    Adds numberOfSections and titleForHeaderInSection methods

--- GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
@@ -9,6 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //MARK: -- Outlets
+    @IBOutlet weak var gotTableView: UITableView!
+    
+    //MARK: -- Data Source Methods
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Season 1"
+        case 1:
+            return "Season 2"
+        case 2:
+            return "Season 3"
+        case 3:
+            return "Season 4"
+        case 4:
+            return "Season 5"
+        case 5:
+            return "Season 6"
+        case 6:
+            return "Season 7"
+        default:
+            return "We don't talk about that one"
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         <#code#>
     }
@@ -17,20 +47,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         <#code#>
     }
     
-    
-    @IBOutlet weak var gotTableView: UITableView!
-    
+    //MARK: -- LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         gotTableView.dataSource = self
         gotTableView.delegate = self
     }
-
-
+    
+    
 }
 
