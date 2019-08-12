d30602718c912fdb963da01259c59225310cb782
Jason Ruan <jasonruan@pursuit.org>
2019-08-12 15:57:12 -0400

    Creates a new folder called 'Views' and creates two new cocoa touch files in it for odd and even seasons

---
+++ GOT-StudentVersion/GOT-StudentVersion/Views/EvenSeasonsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EvenSeasonsTableViewCell.swift
+//  GOT-StudentVersion
+//
+//  Created by Jason Ruan on 8/12/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class EvenSeasonsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
