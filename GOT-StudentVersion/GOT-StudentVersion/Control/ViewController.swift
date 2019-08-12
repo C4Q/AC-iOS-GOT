dfcdfea2ac2bbd34fecbc6827b917a1d5966b9d8
alysonabril <alysonabril@pursuit.org>
2019-08-12 17:47:29 -0400

    season1 test information passed through successfully... yay

--- GOT-StudentVersion/GOT-StudentVersion/Control/ViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/Control/ViewController.swift
@@ -9,21 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     //TODO: connect searchbar, extend searchbardelegate
+    
+    
     //MARK:  --Outlets
     @IBOutlet weak var gotTableView: UITableView!
     
-    //this will return an array, when calling data in function, use indexpath to get the element needed
-    let data = EpisodeData.allEpisodes
+    //will return an array, when calling data in function, use indexpath to get the element needed
+    let data = EpisodeData.season1
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        gotTableView.dataSource = self
-        gotTableView.delegate = self
+        setupProtocols()
     }
-
-
+    
+    
+    
+    private func setupProtocols() {
+        gotTableView.delegate = self
+        gotTableView.dataSource = self
+    }
+    
+    
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
@@ -32,11 +43,17 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = data[indexPath.row].name
-      
-        return cell
+        if let cell = gotTableView.dequeueReusableCell(withIdentifier: "oddCell", for: indexPath) as? oddGOTTVCell {
+            cell.oddTitleLabel?.text = data[indexPath.row].name
+            cell.oddEpisodeInfoLabel?.text = "s:  \(String(data[indexPath.row].season)) ep: \(data[indexPath.row].number)"
+            cell.oddImage.image = UIImage(named: data[indexPath.row].mediumImageID)
+            
+            return cell
+        }
+        return UITableViewCell()
     }
-    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
     
 }
