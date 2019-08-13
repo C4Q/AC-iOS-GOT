18879f61e2d5c8728191885b78715f823ff87313
NormanLiana <liananorman@pursuit.org>
2019-08-13 09:30:38 -0400

    adds custom table view cell, GOTSeasonsTableViewCell

---
+++ GOT-StudentVersion/GOT-StudentVersion/GOTSeasonsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GOTSeasonsTableViewCell.swift
+//  GOT-StudentVersion
+//
+//  Created by Liana Norman on 8/13/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class GOTSeasonsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
