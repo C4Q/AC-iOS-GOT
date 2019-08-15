7efd8ab399ba7c6a0ef7ee76d6f2d26f2798fa7a
aglegaspi <lex83nyc@gmail.com>
2019-08-15 19:00:41 -0400

    removed unneeded code in EpisodeTableViewCell2

--- GOT-StudentVersion/GOT-StudentVersion/EpisodeTableViewCell2.swift
+++ GOT-StudentVersion/GOT-StudentVersion/EpisodeTableViewCell2.swift
@@ -10,21 +10,8 @@ import UIKit
 
 class EpisodeTableViewCell2: UITableViewCell {
     
-    
     @IBOutlet weak var episodeTitle: UILabel!
     @IBOutlet weak var episodeImage: UIImageView!
     @IBOutlet weak var seasonEpisode: UILabel!
-    
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
 }
