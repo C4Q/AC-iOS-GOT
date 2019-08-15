4e2efc4f9e03cc86c23e92601fdf783a43bcf2ec
aglegaspi <lex83nyc@gmail.com>
2019-08-14 20:30:28 -0400

    updated ViewDidLoad in ViewController for access to DataSource and Delegate

--- GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/ViewController.swift
@@ -16,15 +16,17 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
