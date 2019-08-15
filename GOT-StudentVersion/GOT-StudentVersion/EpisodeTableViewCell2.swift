7b41231b2fc1d3c50e23bb6fb7471b67f4eac760
aglegaspi <lex83nyc@gmail.com>
2019-08-14 20:39:00 -0400

    added a new model EpisodeTableViewCell2

---
+++ GOT-StudentVersion/GOT-StudentVersion/EpisodeTableViewCell2.swift
@@ -0,0 +1,24 @@
+//
+//  EpisodeTableViewCell2.swift
+//  GOT-StudentVersion
+//
+//  Created by Alexander George Legaspi on 8/14/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class EpisodeTableViewCell2: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
