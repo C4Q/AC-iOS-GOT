607fe28139519b4d59107e1fa1c67a342a1d5bf0
aglegaspi <lex83nyc@gmail.com>
2019-08-15 20:55:05 -0400

    created outlets in Detail View Controller

--- GOT-StudentVersion/GOT-StudentVersion/DetailViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/DetailViewController.swift
@@ -12,8 +12,18 @@ class DetailViewController: UIViewController {
     
     var got: GOTEpisode!
     
+    @IBOutlet weak var detailImage: UIImageView!
+    @IBOutlet weak var detailTitle: UILabel!
+    @IBOutlet weak var detailSeason: UILabel!
     
-
+    @IBOutlet weak var detailEpisode: UILabel!
+    
+    @IBOutlet weak var detailRuntime: UILabel!
+    
+    @IBOutlet weak var detailAirdate: UILabel!
+    
+    @IBOutlet weak var detailDescription: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
