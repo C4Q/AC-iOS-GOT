b19e19d030d16bf64a7f32833652de0e269b9d92
aglegaspi <lex83nyc@gmail.com>
2019-08-15 21:12:13 -0400

    added properties to ViewDidLoad in Detail View Controller

--- GOT-StudentVersion/GOT-StudentVersion/DetailViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/DetailViewController.swift
@@ -15,30 +15,24 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var detailImage: UIImageView!
     @IBOutlet weak var detailTitle: UILabel!
     @IBOutlet weak var detailSeason: UILabel!
-    
     @IBOutlet weak var detailEpisode: UILabel!
-    
     @IBOutlet weak var detailRuntime: UILabel!
-    
     @IBOutlet weak var detailAirdate: UILabel!
-    
     @IBOutlet weak var detailDescription: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        detailImage.image = UIImage(named: got.originalImageID)
+        detailTitle.text = got.name
+        detailSeason.text = "Season: \(got.season)"
+        detailEpisode.text = "Episode: \(got.number)"
+        detailRuntime.text = "Runtime \(got.runtime)"
+        detailAirdate.text = "Airdate: \(got.airdate)"
+        detailDescription.text = got.summary
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
