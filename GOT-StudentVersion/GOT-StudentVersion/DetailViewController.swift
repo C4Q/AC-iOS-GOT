a2c7c6a2b79baeae91fc4d13062b80a8fde1e709
Jason Ruan <jasonruan@pursuit.org>
2019-08-18 11:38:21 -0400

    Creates a uiviewcontroller file for the detail view controller and adds outlets to it

---
+++ GOT-StudentVersion/GOT-StudentVersion/DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.swift
+//  GOT-StudentVersion
+//
+//  Created by Jason Ruan on 8/18/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var episodeImage: UIImageView!
+    @IBOutlet weak var episodeTItleLabel: UILabel!
+    @IBOutlet weak var seasonLabel: UILabel!
+    @IBOutlet weak var episodeLabel: UILabel!
+    @IBOutlet weak var runtimeLabel: UILabel!
+    @IBOutlet weak var airDateLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
