ccd6df54156c03d5f175159db880a6478bbb38fe
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2019-08-19 22:15:12 -0400

    Backup before constraint rearrangement

--- GOT-StudentVersion/GOT-StudentVersion/Controllers/GOTViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/Controllers/GOTViewController.swift
@@ -51,6 +51,7 @@ class GOTViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
+        
     }
     
     private func configureTableView() {
