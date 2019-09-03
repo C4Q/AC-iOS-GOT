36a04f2e5e4053cb2bc5f59071c7b5d81e196b90
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-09-03 13:19:14 -0400

    creates custom table view cell files for left image cell and right image cell

---
+++ GOT-StudentVersion/GOT-StudentVersion/GoTLeftTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoTLeftTableViewCell.swift
+//  GOT-StudentVersion
+//
+//  Created by Radharani Ribas-Valongo on 9/3/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class GoTLeftTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
