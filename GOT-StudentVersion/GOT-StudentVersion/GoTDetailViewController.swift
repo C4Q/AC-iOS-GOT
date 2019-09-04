83dd6450fe8708fac7aa808bff3e6e991c7e9272
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-09-04 15:26:54 -0400

    creates new detail view controller file and a view controller folder (places detail view controller and main table view controller in that folder)

---
+++ GOT-StudentVersion/GOT-StudentVersion/GoTDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GoTDetailViewController.swift
+//  GOT-StudentVersion
+//
+//  Created by Radharani Ribas-Valongo on 9/4/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class GoTDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
