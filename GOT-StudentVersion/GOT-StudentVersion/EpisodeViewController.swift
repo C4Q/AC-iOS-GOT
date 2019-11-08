b0f0327ec8908728c275c1458b15dc4eca1b934b
Ahad Islam <34593133+ahadislam1@users.noreply.github.com>
2019-11-08 18:51:11 -0500

    completed MVP, tableView as well as passing references and etc.

--- GOT-StudentVersion/GOT-StudentVersion/EpisodeViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/EpisodeViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 
 class EpisodeViewController: UIViewController {
     
+    var episode: GOTEpisode!
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var seasonNumber: UILabel!
     @IBOutlet weak var episodeNumber: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var runTime: UILabel!
-    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.image = UIImage(named: episode.originalImageID)
+        titleLabel.text = episode.name
+        seasonNumber.text = String(episode.season)
+        episodeNumber.text = String(episode.number)
+        dateLabel.text = episode.airdate
+        runTime.text = String(episode.runtime)
+        descriptionTextView.text = episode.summary
         
         // Do any additional setup after loading the view.
     }
--- GOT-StudentVersion/GOT-StudentVersion/GOTViewController.swift
+++ GOT-StudentVersion/GOT-StudentVersion/GOTViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class GotViewCell: UITableViewCell {
+class GOTViewCell: UITableViewCell {
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
@@ -34,41 +34,52 @@ class GOTViewController: UIViewController, UITableViewDelegate, UITableViewDataS
         // Dispose of any resources that can be recreated.
     }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        gotEpisodes[gotEpisodes.count - 1].season
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let episodesInSeason = gotSeasons[section + 1] else { return 0 }
-        return episodesInSeason
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell =  indexPath.section % 2 == 0 ? tableView.dequeueReusableCell(withIdentifier: "Episode Cell", for: indexPath) as! GotViewCell :
-            tableView.dequeueReusableCell(withIdentifier: "Alternate Episode Cell", for: indexPath) as! GotViewCell
-        
-        print("Section: \(indexPath.section), Row: \(indexPath.row)")
-        
-        for episode in gotEpisodes {
-            if indexPath.section == (episode.season - 1) && indexPath.row == (episode.number - 1) {
-                cell.titleLabel?.text = episode.name
-                cell.summaryLabel?.text = "S: \(episode.season) E: \(episode.number)"
-                cell.cellImage?.image = UIImage(named: episode.mediumImageID)
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let tappedTableCell: GOTViewCell = sender as? GOTViewCell {
+            let cellIndexPath = tableView.indexPath(for: tappedTableCell)!
+            if let episodeDestination = segue.destination as? EpisodeViewController {
+                for episode in gotEpisodes {
+                    if cellIndexPath.section == (episode.season - 1) && cellIndexPath.row == (episode.number - 1) {
+                        episodeDestination.episode = episode
+                    }
+                }
             }
         }
-        
-//        cell.detailTextLabel?.text = "S: \(gotEpisodes[indexPath.row].season) E: \(gotEpisodes[indexPath.row].number)"
-//        cell.imageView?.image = UIImage(named: gotEpisodes[indexPath.row].mediumImageID)
-        return cell
     }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        func numberOfSections(in tableView: UITableView) -> Int {
+            gotEpisodes[gotEpisodes.count - 1].season
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            guard let episodesInSeason = gotSeasons[section + 1] else { return 0 }
+            return episodesInSeason
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell =  indexPath.section % 2 == 0 ? tableView.dequeueReusableCell(withIdentifier: "Episode Cell", for: indexPath) as! GOTViewCell :
+                tableView.dequeueReusableCell(withIdentifier: "Alternate Episode Cell", for: indexPath) as! GOTViewCell
+            
+            print("Section: \(indexPath.section), Row: \(indexPath.row)")
+            
+            for episode in gotEpisodes {
+                if indexPath.section == (episode.season - 1) && indexPath.row == (episode.number - 1) {
+                    cell.titleLabel?.text = episode.name
+                    cell.summaryLabel?.text = "S: \(episode.season) E: \(episode.number)"
+                    cell.cellImage?.image = UIImage(named: episode.mediumImageID)
+                }
+            }
+            
+            //        cell.detailTextLabel?.text = "S: \(gotEpisodes[indexPath.row].season) E: \(gotEpisodes[indexPath.row].number)"
+            //        cell.imageView?.image = UIImage(named: gotEpisodes[indexPath.row].mediumImageID)
+            return cell
+        }
+        
+        func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
             return "Season \(section + 1)"
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Cell was selected at \(indexPath.row)")
+        }
         
-    }
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            print("Cell was selected at \(indexPath.row)")
+        }
 }
 
