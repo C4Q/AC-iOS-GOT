6b9024d05f9d6fbd0bec62aa5da53b4ca949cc09
NormanLiana <liananorman@pursuit.org>
2020-01-19 22:23:47 -0500

    adds cocoa files for left and right TV cells to file structure

---
+++ GOT-StudentVersion/GOT-StudentVersion/LeftTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  LeftTVCell.swift
+//  GOT-StudentVersion
+//
+//  Created by Liana Norman on 1/19/20.
+//  Copyright © 2020 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class LeftTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
